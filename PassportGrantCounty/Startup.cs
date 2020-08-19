71b0d5b17c4a2ae90f8f739cb0a0941458639698
Loretta Tappan <lorettatappan@gmail.com>
2020-08-19 09:10:06 -0400

    first commit

---
+++ PassportGrantCounty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PassportGrantCounty.Startup))]
+namespace PassportGrantCounty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
