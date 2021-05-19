18391e9a9335470ae435a6a6d879ab127acc4c1c
Loretta Tappan <lorettatappan@gmail.com>
2021-05-19 08:33:00 -0400

    updated stamps and roadmap views

---
+++ Passport.Data/Migrations/202105170112257_PassportViewsUpdate.cs
@@ -0,0 +1,29 @@
+﻿namespace Passport.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PassportViewsUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.RoadMap", "ChallengeScoreIncrease");
+            DropTable("dbo.Background");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Background",
+                c => new
+                    {
+                        BackgroundId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        AdventureLog = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.BackgroundId);
+            
+            AddColumn("dbo.RoadMap", "ChallengeScoreIncrease", c => c.String(nullable: false));
+        }
+    }
+}
