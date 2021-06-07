61dabb6d6c3d4784e7e8b20d70630d0ce75cbe3c
lorettadtappan <60456050+lorettadtappan@users.noreply.github.com>
2021-06-06 21:04:31 -0400

    updated comments

--- Passport.Data/Entities/Comment.cs
+++ Passport.Data/Entities/Comment.cs
@@ -12,22 +12,19 @@ namespace Passport.Data.Entities
     {
         [Key]
         public int CommentId { get; set; }
+
         [Required]
         public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
         [Required]
         public string Content { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime? LastUpdated { get; set; }
-        [ForeignKey("RoadMap")]
-        public int? RoadMapId { get; set; }
-        public virtual RoadMap RoadMap { get; set; }
-        [ForeignKey("Experience")]
-        public int? ExperienceId { get; set; }
-        public virtual Experience Experience { get; set; }
-        [ForeignKey("Stamp")]
-        public int? StampId { get; set; }
-        public virtual Stamp Stamp { get; set; }
-        public virtual ICollection<Reply> Replies { get; set; }
-        public bool IsDeleted { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
--- Passport.Services/CommentService.cs
+++ Passport.Services/CommentService.cs
@@ -25,8 +25,6 @@ namespace Passport.Services
             {
                 OwnerId = _userId,
                 Content = model.Content,
-                DateCreated = DateTime.Now,
-                RoadMapId = model.ParentId
             };
             _ctx.Comments.Add(entity);
             return _ctx.SaveChanges() == 1;
@@ -38,8 +36,6 @@ namespace Passport.Services
             {
                 OwnerId = _userId,
                 Content = model.Content,
-                DateCreated = DateTime.Now,
-                ExperienceId = model.ParentId
             };
             _ctx.Comments.Add(entity);
             return _ctx.SaveChanges() == 1;
@@ -51,8 +47,6 @@ namespace Passport.Services
             {
                 OwnerId = _userId,
                 Content = model.Content,
-                DateCreated = DateTime.Now,
-                StampId = model.ParentId
             };
             _ctx.Comments.Add(entity);
             return _ctx.SaveChanges() == 1;
@@ -63,7 +57,6 @@ namespace Passport.Services
             var entity = _ctx.Comments.Single(e => e.CommentId == commentId);
             if (entity != null)
             {
-                entity.IsDeleted = true;
             }
             return _ctx.SaveChanges() == 1;
         }
@@ -74,7 +67,6 @@ namespace Passport.Services
             if (entity != null)
             {
                 entity.Content = model.Content;
-                entity.LastUpdated = DateTime.Now;
             }
             return _ctx.SaveChanges() == 1;
         }
@@ -82,15 +74,11 @@ namespace Passport.Services
         public IEnumerable<CommentListModel> GetAllCommentsByRoadMapId(int roadMapId)
         {
             var commentList = _ctx.Comments
-                .Where(e => e.RoadMapId == roadMapId && e.IsDeleted == false)
                 .Select(e => new CommentListModel
                 {
                     CommentId = e.CommentId,
                     Author = _ctx.Users.FirstOrDefault(u => u.Id == e.OwnerId.ToString()).UserName,
                     Content = e.Content,
-                    DateCreated = e.DateCreated,
-                    LastUpdated = e.LastUpdated,
-                    RepliesCount = e.Replies.Where(reply => reply.IsDeleted == false).Count()
                 }).ToList();
             return commentList;
         }
@@ -98,15 +86,11 @@ namespace Passport.Services
         public IEnumerable<CommentListModel> GetAllCommentsByExperienceId(int experienceId)
         {
             var commentList = _ctx.Comments
-                .Where(e => e.ExperienceId == experienceId && e.IsDeleted == false)
                 .Select(e => new CommentListModel
                 {
                     CommentId = e.CommentId,
                     Author = _ctx.Users.FirstOrDefault(u => u.Id == e.OwnerId.ToString()).UserName,
                     Content = e.Content,
-                    DateCreated = e.DateCreated,
-                    LastUpdated = e.LastUpdated,
-                    RepliesCount = e.Replies.Where(reply => reply.IsDeleted == false).Count()
                 }).ToList();
             return commentList;
         }
@@ -114,15 +98,11 @@ namespace Passport.Services
         public IEnumerable<CommentListModel> GetAllCommentsByStampId(int stampId)
         {
             var commentList = _ctx.Comments
-                .Where(e => e.StampId == stampId && e.IsDeleted == false)
                 .Select(e => new CommentListModel
                 {
                     CommentId = e.CommentId,
                     Author = _ctx.Users.FirstOrDefault(u => u.Id == e.OwnerId.ToString()).UserName,
                     Content = e.Content,
-                    DateCreated = e.DateCreated,
-                    LastUpdated = e.LastUpdated,
-                    RepliesCount = e.Replies.Where(reply => reply.IsDeleted == false).Count()
                 }).ToList();
             return commentList;
         }
@@ -134,10 +114,7 @@ namespace Passport.Services
             {
                 Author = _ctx.Users.FirstOrDefault(u => u.Id == entity.OwnerId.ToString()).UserName,
                 Content = entity.Content,
-                DateCreated = entity.DateCreated,
-                LastUpdated = entity.LastUpdated,
                 CommentId = entity.CommentId,
-                RepliesCount = entity.Replies.Where(reply => reply.IsDeleted == false).Count()
             };
             return model;
         }
